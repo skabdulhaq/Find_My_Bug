b4c3b6fee07613f839f5b6e09123f8d932c5052f
sk.abdul haq <75989562+skabdulhaq@users.noreply.github.com>
2021-12-10 16:48:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+app = Flask(__name__)
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email',
+                        validators=[DataRequired(),
+                                    Email(message="Invalid Email Address",
+                                          check_deliverability=True)])
+    password = PasswordField(label='Password',
+                             validators=[DataRequired(),
+                                         Length(min=8,
+                                                message="Password Should Be Between 8-30 Characters")])
+    login = SubmitField(label="Login")
+
+app.secret_key = "hi"
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login_page():
+    login_form = LoginForm(app.secret_key)
+    if request.method == "POST":
+        print(f"{request.method}")
+        print(f"{login_form.validate_on_submit()}")
+        print(f"{login_form.email.data} ")
+        print(f"{login_form.password.data} ")
+    return render_template('login.html', form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
